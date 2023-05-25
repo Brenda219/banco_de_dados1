463afed756d5b4da2facea3e7719d01cf9dc2285
Brenda avelino <117834902+Brenda219@users.noreply.github.com>
2023-05-25 20:43:47 -0300

    Update vendas produtos1.sql

--- vendas produtos1.sql
+++ vendas produtos1.sql
@@ -1,36 +1,15 @@
 CREATE DATABASE vendas_produtos1;
 use vendas_produtos1;
 
-
 create table cliente
 (
   id_cliente int not null,
   nome_cliente varchar (40),
   cpf_cliente int,
   primary key (id_cliente)
-);
+
+ ); 
   
-  create table produto
-  (
-  id_produto int not null,
-  nome_produto varchar(20),
-  data_validade_produto int,
-  primary key (id_produto)
-  );
-  
-  create table venda
-  (
-  id_venda int not null,
-  valor_venda int,
-  data_venda int,
-  id_cliente int,
-  id_vendedor int,
-  id_pagamento int,
-  primary key (id_venda),
-  foreign key (id_cliente) references cliente(id_cliente),
-  foreign key (id_vendedor) references vendedor(id_vendedor),
-  foreign key(id_pagamento) references pagamento(id_pagamento)
-  );
   
   create table venda_produto
   (
@@ -51,16 +30,51 @@ primary key(id_pagamento)
  id_vendedor int,
  nome_vendedor varchar(20),
  primary key(id_vendedor)
+ 
+);
+create table categoria 
+(
+id_categoria int,
+nome_categoria varchar (10),
+primary key (id_categoria)
+);
+ create table fornecedor 
+ (
+ id_fornecedor int,
+ cnpj varchar(10),
+ nome varchar(10),
+ primary key(id_fornecedor)
+ 
+ );
+ create table produto
+  (
+  id_produto int not null,
+  nome_produto varchar(20),
+  data_validade_produto int,
+ id_categoria int,
+  id_fornecedor int,
+  primary key (id_produto)
  );
 
-insert into produto(id_produto,nome_produto)
-values(1,'meia,cueca');
- 
- 
- insert into venda(id_venda,valor_venda)
- values(1,'200R$')
+ create table venda
+  (
+  id_venda int not null,
+  valor_venda int,
+  data_venda int,
+  id_cliente int,
+  id_vendedor int,
+  id_pagamento int,
+  primary key (id_venda)
+  );
 
-
+alter table produto
+ add  foreign key (id_categoria)references categoria(id_categoria),
+ add foreign key (id_fornecedor)references fornecedor(id_fornecedor);
+ 
+ alter table venda
+ add foreign key (id_cliente)references cliente(id_cliente),
+ add  foreign key (id_vendedor)references vendedor(id_vendedor),
+  add  foreign key (id_pagamento)references pagamento(id_pagamento);
 
 
 
